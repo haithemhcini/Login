845d9257de111764b6997d3de2bafdbe04d7a6fb
haithemhcini <haithem.hcini@esprit.tn>
2021-05-08 20:39:59 +0100

    Final

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,11 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular11JwtAuth'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular11JwtAuth');
-  });
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
